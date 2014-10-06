d69b8d13fd025a9a5383aa08a0cab2281603518b
Christophe Masson <christophe.masson@supersonicimagine.com>
2014-10-06 12:52:46 +0200

    Handle the options in the UNIX way.

--- createBuild.sh
+++ createBuild.sh
@@ -1,42 +1,123 @@
 #!/bin/bash
 
-## default is SSITeam
-vindinium_key=uviqc5an
-navigator=""
+URL=""
+KEY="uviqc5an" # Default is SSITeam
+MODE=""
+NB_TURNS=""
+MAP=""
+BROWSER=""
 
-if [ $# -eq 0 ]
-then
-    ### just use the default !
-    echo "use SSITeam key!"
-elif [ $# -eq 1 ]
-then
-    vindinium_key=$1
-elif [ $# -eq 2 ]
-then
-    vindinium_key=$1
-    navigator=$2
-else
-    echo "Syntax: $0 vindinium_key [navigator]"
-    exit 0
-fi
+################################################################################
+usage()
+{
+    prog_name=`basename $0`
+    echo "Usage: $prog_name [<options>]"
+    echo ""
+    echo "Options:"
+    echo "  -h"
+    echo "      Print this help message."
+    echo ""
+    echo "  -u <url>"
+    echo "      The URL of the Vindinium server."
+    echo ""
+    echo "  -k <key>"
+    echo "      Your secret key given on the Vindinium server."
+    echo "      Default ID is SSITeam."
+    echo ""
+    echo "  -a <mode>"
+    echo "      The game mode (API) to start."
+    echo "      Available modes: training (0) or arena (1)."
+    echo "      Default: training"
+    echo ""
+    echo "  -m <map>"
+    echo "      The map to play with."
+    echo "      Available maps: m1..m6."
+    echo "      Only available for the training mode."
+    echo ""
+    echo "  -n <nb turns>"
+    echo "      The number of turns to play."
+    echo "      Only available for the training mode."
+    echo ""
+    echo "  -b <behaviour>"
+    echo "      The behavior module you want to activate for this game."
+    echo ""
+    echo "  -w <browser>"
+    echo "      The browser your want to open to display the game."
+    echo ""
+}
+
+################################################################################
+parse_opt()
+{
+    while getopts ":hu:k:a:m:n:w:" option
+    do
+        case $option in
+            h) usage ; exit 0 ;;
+            u) URL=${OPTARG} ;;
+            k) KEY=${OPTARG} ;;
+            a) MODE=${OPTARG} ;;
+            m) MAP=${OPTARG} ;;
+            n) NB_TURNS=${OPTARG} ;;
+            w) BROWSER=${OPTARG} ;;
+            *) usage ; exit 1 ;;
+        esac
+    done
+}
+
+################################################################################
+parse_opt $@
+shift $(($OPTIND - 1))
 
 rm -rf build
 mkdir -p build/dump
 cd build
 
-echo "#!/bin/bash" > testIA.sh
-echo "../bin/vindiniumLauncher $vindinium_key random $navigator" >> testIA.sh
-chmod +x testIA.sh
+CMD_LINE="../bin/vindiniumLauncher"
+if [ -n "$URL" ] ; then
+    CMD_LINE="$CMD_LINE --url=$URL"
+fi
+if [ -n "$MODE" ] ; then
+    CMD_LINE="$CMD_LINE --mode=$MODE"
+fi
+if [ -n "$MAP" ] ; then
+    CMD_LINE="$CMD_LINE --map=$MAP"
+fi
+if [ -n "$TURNS" ] ; then
+    CMD_LINE="$CMD_LINE --turns=$TURNS"
+fi
+if [ -n "$BROWSER" ] ; then
+    CMD_LINE="$CMD_LINE --browser=$BROWSER"
+fi
 
-echo "#!/bin/bash" > mitchIA.sh
-echo "../bin/vindiniumLauncher $vindinium_key botMitch $navigator" >> mitchIA.sh
-chmod +x mitchIA.sh
+# random
+cat > testIA.sh << EOF
+#!/bin/sh
 
-echo "#!/bin/bash" > TheRalphBot.sh
-echo "../bin/vindiniumLauncher $vindinium_key TheRalphBot $navigator" >> TheRalphBot.sh
-chmod +x TheRalphBot.sh
+$CMD_LINE --behaviour=random -- $KEY
+
+EOF
+chmod u+x testIA.sh
+
+# botMitch
+cat > mitchIA.sh << EOF
+#!/bin/sh
+
+$CMD_LINE --behaviour=botMitch -- $KEY
+
+EOF
+chmod u+x mitchIA.sh
+
+# TheRalphBot
+cat > TheRalphBot.sh << EOF
+#!/bin/sh
+
+$CMD_LINE --behaviour=TheRalphBot -- $KEY
+
+EOF
+chmod u+x TheRalphBot.sh
 
 ln -s ../bin/vindiniumLauncher vindiniumLauncher
 
 cmake ..
 cd ..
+
--- vindiniumLauncher.cpp
+++ vindiniumLauncher.cpp
@@ -4,6 +4,11 @@
 #include <iostream>
 #include <iomanip>
 
+#include <stdlib.h>
+#include <unistd.h>
+#include <libgen.h>
+#include <getopt.h>
+
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 /// Local includes
@@ -12,90 +17,221 @@
 
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
+struct SParameters
+{
+    std::string            m_url;
+    std::string            m_key;
+    MOBE::E_VINDINIUM_MODE m_mode;
+    std::string            m_map;
+    int                    m_turns;
+    std::string            m_behaviour;
+    std::string            m_browser;
+};
+
+static char *short_options = (char *)"hu:a:m:n:b:w:";
+static struct option long_options[] =
+{
+    {"help",      no_argument,       NULL, 'h'},
+    {"url",       required_argument, NULL, 'u'},
+    {"mode",      required_argument, NULL, 'a'},
+    {"map",       required_argument, NULL, 'm'},
+    {"turns",     required_argument, NULL, 'n'},
+    {"behaviour", required_argument, NULL, 'b'},
+    {"browser",   required_argument, NULL, 'w'},
+    {0, 0, 0, 0}
+};
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 void displayHelp(const std::string& inExeName)
 {
-    std::string exeName(inExeName);
-    std::string::size_type idOfPoint = inExeName.find_last_of('/');
-    if (idOfPoint != std::string::npos)
-    {
-        exeName = inExeName.substr(idOfPoint+1);
-    } else {}
-//TODO    std::cout<<"Syntax: "<<exeName<<" vindinium_key behavior_name [mode] [nb_turns] [map] [navigator]"<<std::endl;
-    std::cout<<"Syntax: "<<exeName<<" vindinium_key behavior_name [navigator]"<<std::endl;
-    std::cout<<"        "<<exeName<<" --help"<<std::endl;
-    std::cout<<std::endl;
-    std::cout<<"'vindinium_key' is your secret key given on the web site vindinium.org."<<std::endl;
-    std::cout<<"'behavior_name' is the name of the behavior module you want to activate for this game."<<std::endl;
-//TODO    std::cout<<"'mode'          is the game mode to start: ARENA or TRAINING."<<std::endl;
-//TODO    std::cout<<"'nb_turns        is the number of turn to play. Only available for TRAINING mode."<<std::endl;
-//TODO    std::cout<<"'map            is the map to play with. 6 maps are available: m1 m2 m3 m4 m5 m6. Only available for TRAINING mode."<<std::endl;
-    std::cout<<"'navigator'     is the name of the navigator your want to open to didplay the game. No navigator by default."<<std::endl;
-    std::cout<<std::endl;
+    std::cout << "Usage: " << inExeName << " [options] <key>"                     << std::endl;
+    std::cout                                                                     << std::endl;
+    std::cout << "Argument:"                                                      << std::endl;
+    std::cout << "  <key>"                                                        << std::endl;
+    std::cout << "      Your secret key given on the Vindinium server."           << std::endl;
+    std::cout                                                                     << std::endl;
+    std::cout << "Options:"                                                       << std::endl;
+    std::cout << "  -h, --help"                                                   << std::endl;
+    std::cout << "      Print this help message."                                 << std::endl;
+    std::cout                                                                     << std::endl;
+    std::cout << "  -u <url>, --url=<url>"                                        << std::endl;
+    std::cout << "      The URL of the Vindinium sever."                          << std::endl;
+    std::cout << "      Default: \"vindinium.org\"."                              << std::endl;
+    std::cout                                                                     << std::endl;
+    std::cout << "  -a <mode>, --mode=<mode>"                                     << std::endl;
+    std::cout << "      The game mode (API) to start."                            << std::endl;
+    std::cout << "      Available modes: training (0) and arena (1)."             << std::endl;
+    std::cout << "      Default: training."                                       << std::endl;
+    std::cout                                                                     << std::endl;
+    std::cout << "  -m <map>, --map=<map>"                                        << std::endl;
+    std::cout << "      The map to play with."                                    << std::endl;
+    std::cout << "      Only available for the training mode."                    << std::endl;
+    std::cout << "      Available maps : m1..m6."                                 << std::endl;
+    std::cout << "      Default: m6."                                             << std::endl;
+    std::cout                                                                     << std::endl;
+    std::cout << "  -n <nb turns>, --turns=<nb turns>"                            << std::endl;
+    std::cout << "      The number of turns to play."                             << std::endl;
+    std::cout << "      Only available for the training mode."                    << std::endl;
+    std::cout << "      Default: -1."                                             << std::endl;
+    std::cout                                                                     << std::endl;
+    std::cout << "  -b <behaviour>, --behaviour=<behaviour>"                      << std::endl;
+    std::cout << "      The behavior module you want to activate for this game."  << std::endl;
+    std::cout << "      Default: random"                                          << std::endl;
+    std::cout                                                                     << std::endl;
+    std::cout << "  -w <browser>, --browser=<browser>"                            << std::endl;
+    std::cout << "      The browser your want to open to display the game."       << std::endl;
+    std::cout << "      Default: none."                                           << std::endl;
+    std::cout                                                                     << std::endl;
+
+    // Behaviour
     MOBE::CBehaviorMgr behaviorMrg;
     behaviorMrg.loadBehaviorModules();
     const MOBE::behaviorsMap_t &behaviors = behaviorMrg.getBehaviors();
-    std::cout<<"Nb available behaviors: "<<behaviors.size()<<std::endl;
-    std::cout<<std::setw(20)<<std::left<<"behavior_name"<<"comment"<<std::endl;
-    std::cout<<std::setw(20)<<std::left<<"-------------"<<"-------"<<std::endl;
+    std::cout << "Number of available behaviours: " << behaviors.size() << std::endl;
+    std::cout << std::endl;
+    std::cout << std::setw(20) << std::left << "behavior_name"  << "comment" << std::endl;
+    std::cout << std::setw(20) << std::left << "-------------" << "-------" << std::endl;
     for (auto &behaviorIt : behaviors)
     {
         MOBE::IBehaviorModule *pBehavior = behaviorIt.second;
-        std::cout<<std::setw(20)<<std::left<<pBehavior->getName()<<pBehavior->getComment()<<std::endl;
+        std::cout << std::setw(20) << std::left
+                  << pBehavior->getName() << pBehavior->getComment() << std::endl;
     } // for
-    std::cout<<std::setw(20)<<std::left<<"-------------"<<"-------"<<std::endl;
-
-//TODO use --vindinium_key, --behavior_name, --mode, --nb_turn, --map, --navigator
+    std::cout << std::setw(20) << std::left << "-------------" << "-------" << std::endl;
+    std::cout << std::endl;
 } // displayHelp
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+void parse_options(const int argc, char **argv, struct SParameters& out_parameters)
+{
+    std::string exeName = std::string(basename(argv[0]));
+
+    if (argc == 1)
+    {
+        displayHelp(exeName);
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        char opt;
+
+        do
+        {
+            opt = getopt_long(argc, argv, short_options, long_options, NULL);
+
+            switch(opt)
+            {
+                case 'h':
+                    displayHelp(exeName);
+                    exit(EXIT_SUCCESS);
+                    break;
+
+                case 'u':
+                    out_parameters.m_url = std::string(optarg);
+                    break;
+
+                case 'k':
+                    out_parameters.m_key = std::string(optarg);
+                    break;
+
+                case 'b':
+                    out_parameters.m_behaviour = std::string(optarg);
+                    break;
+
+                case 'a':
+                    {
+                        int mode = atoi(optarg);
+                        if ((mode >= 0) && (mode < MOBE::NB_VINDINIUM_MODE))
+                        {
+                            out_parameters.m_mode = (MOBE::E_VINDINIUM_MODE)mode;
+                        }
+                        else
+                        {
+                            std::cerr << "Invalid mode" << std::endl;
+                            exit(EXIT_FAILURE);
+                        }
+                    }
+                    break;
+
+                case 'n':
+                    out_parameters.m_turns = atoi(optarg);
+                    break;
+
+                case 'm':
+                    out_parameters.m_map = std::string(optarg);
+                    break;
+
+                case 'w':
+                    out_parameters.m_browser = std::string(optarg);
+                    break;
+
+                case -1:
+                    break;
+
+                case '?':
+                    displayHelp(exeName);
+                    exit(EXIT_FAILURE);
+                    break;
+
+                default:
+                    abort();
+                    break;
+            }
+        }
+        while (opt != -1);
+
+        // One argument after options: the key
+        if (optind != argc - 1)
+        {
+            displayHelp(exeName);
+            exit(EXIT_FAILURE);
+        }
+        else
+        {
+            out_parameters.m_key = std::string(argv[optind]);
+        }
+    }
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 int main(int inArgC, char** inpArgV)
 {
-    std::string exeName(inpArgV[0]);
-    if (inArgC < 2 || inArgC > 4)
+    struct SParameters parameters =
     {
-        displayHelp(exeName);
+        .m_url =        "vindinium.org",
+        .m_key =        "",
+        .m_mode =       MOBE::E_VINDINIUM_TRAINING_MODE,
+        .m_map =        "m6",
+        .m_turns =      -1,
+        .m_behaviour =  "random",
+        .m_browser =    ""
+    };
+
+    parse_options(inArgC, inpArgV, parameters);
+
+    MOBE::CClient vdcClient(parameters.m_key,
+                            parameters.m_url,
+                            parameters.m_behaviour,
+                            parameters.m_browser);
+
+    if (!vdcClient.startGame(parameters.m_mode,
+                             parameters.m_turns,
+                             parameters.m_map))
+    {
+        std::cerr << "Cannot start game in training mode with key '"
+                  << vdcClient.getKey()
+                  << "' on server '" << vdcClient.getServerHostName()
+                  << "'" << std::endl;
     }
     else
     {
-        int nbTurns = -1;
-        std::string vindiniumKey(inpArgV[1]),
-                    vindiniumURL("vindinium.org"),
-                    activeBehavior(""),
-                    navigator(""),
-                    gameMap("m6");
-
-//        MOBE::E_VINDINIUM_MODE gameMode = MOBE::E_VINDINIUM_TRAINING_MODE;
-        MOBE::E_VINDINIUM_MODE gameMode = MOBE::E_VINDINIUM_ARENA_MODE;
-
-        if (vindiniumKey == "--help")
-        {
-            displayHelp(exeName);
-        }
-        else
-        {
-            if (inArgC >= 3)
-            {
-                activeBehavior=inpArgV[2];
-            } else {}
-
-            if (inArgC >= 4)
-            {
-                navigator=inpArgV[3];
-            } else {}
-
-            MOBE::CClient vdcClient(vindiniumKey, vindiniumURL, activeBehavior, navigator);
-            if (!vdcClient.startGame(gameMode, nbTurns, gameMap))
-            {
-                std::cerr<<"Can not start game in Trainig mode with key '"<<vdcClient.getKey()<<"' on server '"<<vdcClient.getServerHostName()<<"'"<<std::endl;
-            }
-            else
-            {
-                // End of the game
-            } // else
-        } // else
+        // End of the game
     } // else
 
-    std::cout<<"Bye bye!!!"<<std::endl;
+    std::cout << "Bye bye!!!" << std::endl;
     return 0;
 } // main
+
